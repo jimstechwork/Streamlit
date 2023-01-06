99741046852b26b0cace471819c9b0330420c836
jimstechwork <109333969+jimstechwork@users.noreply.github.com>
2023-01-06 12:57:25 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -40,3 +40,4 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data1), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+    st.dataframe(df)
